7254b0a722f47cf2e8fed4c21356839d4baa9647
James Nguyen <jnguyen@starbucks.com>
2022-06-04 22:24:25 -0700

    update

--- sensors/sensor1.py
+++ sensors/sensor1.py
@@ -15,10 +15,10 @@ class HelloSensor(Sensor):
     def run(self):
         while not self._stop:
             self._logger.debug("HelloSensor dispatching trigger...")
-            count = self.sensor_service.get_value("learn_stackstorm.count") or 0
+            count = self.sensor_service.get_value("learnst2.count") or 0
             payload = {"greeting": "Yo, StackStorm!", "count": int(count) + 1}
-            self.sensor_service.dispatch(trigger="learn_stackstorm.event1", payload=payload)
-            self.sensor_service.set_value("learn_stackstorm.count", payload["count"])
+            self.sensor_service.dispatch(trigger="learnst2.event1", payload=payload)
+            self.sensor_service.set_value("learnst2.count", payload["count"])
             eventlet.sleep(60)
 
     def cleanup(self):
